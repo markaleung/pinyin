4540982b96d43cd34e57b12f535b7108c4cd575d
Mark Leung <markaleung@gmail.com>
2024-05-04 22:11:07 +0800

    Streamlit - Escape Markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-import streamlit as st
+import streamlit as st, re
 from classes import annotate
 
 one_text = annotate.OneText()
@@ -6,4 +6,4 @@ one_text.config.newline = '  \n' # Markdown newline needs 2 spaces
 one_text.config.text = st.text_area("Enter Chinese Text Here")
 one_text.config.language = st.selectbox('Choose a Language', ['cantonese', 'mandarin'])
 st.markdown('Chinese Text With Romanisation')
-st.text(one_text.main())
+st.markdown(re.sub(r'[#_*\\<>]', r'\\\1', one_text.main()))
