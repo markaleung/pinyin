11c147351e7cf35cb488f7890a4ea744b9a4e726
Mark Leung <markaleung@gmail.com>
2024-05-04 22:04:51 +0800

    Streamlit - Don't use markdown

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ one_text.config.newline = '  \n' # Markdown newline needs 2 spaces
 one_text.config.text = st.text_area("Enter Chinese Text Here")
 one_text.config.language = st.selectbox('Choose a Language', ['cantonese', 'mandarin'])
 st.markdown('Chinese Text With Romanisation')
-st.markdown(one_text.main())
+st.text(one_text.main())
