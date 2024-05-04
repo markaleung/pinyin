4cd79c61c35d4031422f3792628b53e266aedabc
Mark Leung <markaleung@gmail.com>
2024-05-05 07:20:28 +0800

    Annotate - Add boolean config for splitting text on punctuation - Add punctuation characters to config Streamlit - Add boolean config for splitting text on punctuation

--- classes/annotate.py
+++ classes/annotate.py
@@ -3,6 +3,9 @@ import jyutping, re, os, pinyin, tqdm
 class Config:
     def __init__(self):
         self.newline = '\n'
+        self.split_text_on_punctuation = True
+        self.cantonese_multiple_pronunciation = True
+        self.chinese_punctuation = r'([，。？])'
         # language, folder, filename, text, line, char must be added
 
 class OneChar:
@@ -11,6 +14,7 @@ class OneChar:
     def _translate(self):
         pass
     def main(self):
+        # Detect Chinese characters
         if re.search(u'[\u4e00-\u9fff]', self.config.char):
             self._translate()
             return self.output + ' '
@@ -18,15 +22,18 @@ class OneChar:
             return self.config.char
 class Cantonese(OneChar):
     def _translate(self):
-        self.output = jyutping.get(self.config.char, multiple = self.config.cantonese_multiple)[0]
-        if self.config.cantonese_multiple:
+        self.output = jyutping.get(self.config.char, multiple = self.config.cantonese_multiple_pronunciation)[0]
+        if self.config.cantonese_multiple_pronunciation:
+            # Invalid character is empty set
             if len(self.output) == 0:
                 self.output = self.config.char
             elif len(self.output) > 1:
                 self.output = f'({"".join(self.output)})'
+            # Even single pronunciation is a set
             elif len(self.output) == 1:
                 self.output = list(self.output)[0]
         else:
+            # No sets for single pronunciation
             if self.output is None:
                 self.output = self.config.char
 class Mandarin(OneChar):
@@ -58,7 +65,8 @@ class OneText:
         self.config = Config()
         self.lines = []
     def _split_text(self):
-        self.config.text = re.sub(r'([，。])', r'\1\n', self.config.text)
+        if self.config.split_text_on_punctuation:
+            self.config.text = re.sub(self.config.chinese_punctuation, r'\1\n', self.config.text)
     def _run_line(self):
         self.one_line = OneLine(config_ = self.config)
         self.lines.append(self.one_line.main())
@@ -111,6 +119,3 @@ MAPPING = {
     'cantonese': 'jyutping', 
     'mandarin': 'pinyin', 
 }
-if __name__=='__main__':
-    multi = MultiFile()
-    multi.main()
