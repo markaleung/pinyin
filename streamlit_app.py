6a62994241cbc65324e96f7828c45db2a7fbaa4c
Mark Leung <markaleung@gmail.com>
2024-05-04 21:46:03 +0800

    Streamlit - Tidy UI - Add comment

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 from classes import annotate
 
 one_text = annotate.OneText()
-one_text.config.newline = '  \n'
+one_text.config.newline = '  \n' # Markdown newline needs 2 spaces
 one_text.config.text = st.text_area("Enter Chinese Text Here")
-one_text.config.language = st.selectbox('Choose a language', ['cantonese', 'mandarin'])
+one_text.config.language = st.selectbox('Choose a Language', ['cantonese', 'mandarin'])
 st.markdown('Chinese Text With Romanisation')
 st.markdown(one_text.main())
