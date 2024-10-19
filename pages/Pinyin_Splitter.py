6cb614e44818590973c717e2b1feca7d56f80d9c
Mark Leung <markaleung@gmail.com>
2024-10-19 21:14:22 +0800

    Splitter streamlit: Convert to class, add examples

--- pages/Pinyin_Splitter.py
+++ pages/Pinyin_Splitter.py
@@ -3,10 +3,34 @@ os.chdir(os.path.dirname(__file__)+'/..')
 from classes import pivot
 import streamlit as st
 
-pivot = pivot.Pivot()
-pivot.column = st.selectbox('Character Part', ['Initial', 'Rhyme'])
-pivot.pinyin = st.selectbox('Language', ['Cantonese', 'Mandarin'])
-unique_values = sorted(pivot.df_raw[f'{pivot.column}_{pivot.pinyin}'].unique())
-pivot.value = st.selectbox('Value', unique_values)
-pivot.main()
-st.dataframe(pivot.df)
+class Manager:
+    def _select_character_language(self):
+        self.pivot = pivot.Pivot()
+        self.pivot.column = st.selectbox('Character Part', ['Rhyme', 'Initial'])
+        self.pivot.pinyin = st.selectbox('Language', ['Mandarin', 'Cantonese'])
+    def _select_value(self):
+        self.segment = f'{self.pivot.column}_{self.pivot.pinyin}'
+        self.unique_values = sorted(self.pivot.df_raw[self.segment].unique())
+        self.pivot.value = st.selectbox('Value', self.unique_values)
+    def _run_pivot(self):
+        self.pivot.main()
+        st.dataframe(self.pivot.df)
+    def _filter_examples(self):
+        self.column_name = st.selectbox(self.pivot.df.columns.name, self.pivot.df.columns)
+        self.df_filtered = self.pivot.df.T.query(f'{self.pivot.df.columns.name} == @self.column_name').T.dropna()
+        self.index_name = st.selectbox(self.df_filtered.index.name, self.df_filtered.index)
+    def _run_examples(self):
+        st.dataframe(self.pivot.df_raw.query(
+            f'{self.segment} == @self.pivot.value ' 
+            f'and {self.pivot.df.columns.name} == @self.column_name '
+            f'and {self.pivot.df.index.name} == @self.index_name '
+        )[self.pivot.df_raw.columns[0:3]])
+    def main(self):
+        self._select_character_language()
+        self._select_value()
+        self._run_pivot()
+        self._filter_examples()
+        self._run_examples()
+
+manager = Manager()
+manager.main()
