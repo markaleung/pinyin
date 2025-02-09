0bebf9d963e7c1c7d212cda9c538291be84d54f7
Mark Leung <markaleung@gmail.com>
2025-02-09 17:03:08 +0800

    Add Links page

---
+++ pages/Pinyin Links.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.write('''
+# Pleco
+- [Pleco Software - Chinese dictionary app for iOS and Android](https://www.pleco.com)
+- [How to make Pleco into a Cantonese power tool](https://blog.cantoblog.com/2019/11/how-to-make-pleco-into-cantonese-power.html)
+# Other
+- [Cantonese Font: Chinese characters with Jyutping](https://visual-fonts.com)
+- [TypeDuck: type Chinese characters with Jyutping](https://www.typeduck.hk)
+- [Words.hk: Cantonese dictionary](https://words.hk)
+- [Zidou: Cantonese wordle](https://chaaklau.github.io/zidou/)
+''')
