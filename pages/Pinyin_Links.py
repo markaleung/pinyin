c301e999699321af9ca54a52e3ee48426a6b48ea
Mark Leung <markaleung@gmail.com>
2025-03-15 07:52:48 +0800

    Links: books, courses

--- pages/Pinyin_Links.py
+++ pages/Pinyin_Links.py
@@ -3,7 +3,11 @@ st.write('''
 # Pleco
 - [Pleco Software - Chinese dictionary app for iOS and Android](https://www.pleco.com)
 - [How to make Pleco into a Cantonese power tool](https://blog.cantoblog.com/2019/11/how-to-make-pleco-into-cantonese-power.html)
+# Books
+- [Cantonese for Everyone](https://www.goodreads.com/book/show/42300056-cantonese-for-everyone)
+- [Cantonese: A Comprehensive Grammar]()]https://www.cuhk.edu.hk/lin/cbrc/CantoneseGrammar/)
 # Other
+- [CUHK Courses](https://yccla.cuhk.edu.hk/ces-index)
 - [Cantonese Font: Chinese characters with Jyutping](https://visual-fonts.com)
 - [TypeDuck: type Chinese characters with Jyutping](https://www.typeduck.hk)
 - [Words.hk: Cantonese dictionary](https://words.hk)
