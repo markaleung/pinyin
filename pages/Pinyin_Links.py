aae8348019c9f5429480dad42915e31cfbdc53c7
Mark Leung <markaleung@gmail.com>
2025-03-29 17:12:38 +0800

    Update links

--- pages/Pinyin_Links.py
+++ pages/Pinyin_Links.py
@@ -1,15 +1,17 @@
 import streamlit as st
 st.write('''
 # Pleco
-- [Pleco Software - Chinese dictionary app for iOS and Android](https://www.pleco.com)
+- [Pleco Software](https://www.pleco.com) - Chinese dictionary app for iOS and Android
 - [How to make Pleco into a Cantonese power tool](https://blog.cantoblog.com/2019/11/how-to-make-pleco-into-cantonese-power.html)
 # Books
 - [Cantonese for Everyone](https://www.goodreads.com/book/show/42300056-cantonese-for-everyone)
-- [Cantonese: A Comprehensive Grammar]()]https://www.cuhk.edu.hk/lin/cbrc/CantoneseGrammar/)
+- [Cantonese: A Comprehensive Grammar](https://www.cuhk.edu.hk/lin/cbrc/CantoneseGrammar/)
+# Activities
+- [Yale-China Chinese Language Academy](https://yccla.cuhk.edu.hk/ces-index) - Cantonese/Mandarin Courses at the Chinese University of Hong Kong
+- [Cantonese English Study Club](https://www.meetup.com/hong-kong-cantonese-english-language-exchange-club/) - Weekly 1 on 1 language exchange at Starbucks
 # Other
-- [CUHK Courses](https://yccla.cuhk.edu.hk/ces-index)
-- [Cantonese Font: Chinese characters with Jyutping](https://visual-fonts.com)
-- [TypeDuck: type Chinese characters with Jyutping](https://www.typeduck.hk)
-- [Words.hk: Cantonese dictionary](https://words.hk)
-- [Zidou: Cantonese wordle](https://chaaklau.github.io/zidou/)
+- [Cantonese Font](https://visual-fonts.com) - Chinese characters with Jyutping
+- [TypeDuck](https://www.typeduck.hk) - Type Chinese characters with Jyutping
+- [Words.hk](https://words.hk) - Cantonese dictionary
+- [Zidou](https://chaaklau.github.io/zidou/) - Cantonese wordle
 ''')
