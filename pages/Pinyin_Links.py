43e46458f989641936084961c741bc68bb3a0a71
Mark Leung <markaleung@gmail.com>
2025-05-10 16:09:30 +0800

    Links: add Android/iOS/Mac

--- pages/Pinyin_Links.py
+++ pages/Pinyin_Links.py
@@ -9,9 +9,13 @@ st.write('''
 # Activities
 - [Yale-China Chinese Language Academy](https://yccla.cuhk.edu.hk/ces-index) - Cantonese/Mandarin Courses at the Chinese University of Hong Kong
 - [Cantonese English Study Club](https://www.meetup.com/hong-kong-cantonese-english-language-exchange-club/) - Weekly 1 on 1 language exchange at Starbucks
-# Other
+# Type Chinese characters with Jyutping
+- [Android](https://www.cantonese.tools/android.html)
+- [iOS](https://www.cantonese.tools/ios.html)
+- [Mac](https://www.cantonese.tools/mac.html)
+- [TypeDuck](https://www.typeduck.hk)
+# Other Tools
 - [Cantonese Font](https://visual-fonts.com) - Chinese characters with Jyutping
-- [TypeDuck](https://www.typeduck.hk) - Type Chinese characters with Jyutping
 - [Words.hk](https://words.hk) - Cantonese dictionary
 - [Zidou](https://chaaklau.github.io/zidou/) - Cantonese wordle
 ''')
