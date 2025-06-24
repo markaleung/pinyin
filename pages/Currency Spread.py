b800de828b6f35125399cd059a43743afb9b7185
Mark Leung <markaleung@gmail.com>
2025-06-25 06:36:23 +0800

    Currency: add float

--- pages/Currency Spread.py
+++ pages/Currency Spread.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-low = st.number_input('Low', value = 100, min_value = 0)
-high = st.number_input('High', value = 100, min_value = low)
+low = st.number_input('Low', value = 100.0, min_value = 0.0)
+high = st.number_input('High', value = 100.0, min_value = low)
 diff = high - low
 spread = diff / high
 output = f'''
