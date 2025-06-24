2cf8bc4039376e52c49a3cd66cb9ec39e1911536
Mark Leung <markaleung@gmail.com>
2025-06-25 06:35:01 +0800

    Add Spread Calculator

---
+++ pages/Currency Spread.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+low = st.number_input('Low', value = 100, min_value = 0)
+high = st.number_input('High', value = 100, min_value = low)
+diff = high - low
+spread = diff / high
+output = f'''
+```python
+Diff   = {high} - {low} = {diff}
+Spread = {diff} / {high} = {round(spread, 6)}
+```
+'''
+st.write(output)
