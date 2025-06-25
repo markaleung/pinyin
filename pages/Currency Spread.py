7bf4b403d6df811b80a8a0ae86d020f956629fdf
Mark Leung <markaleung@gmail.com>
2025-06-25 20:28:35 +0800

    Currency Spread: format it

--- pages/Currency Spread.py
+++ pages/Currency Spread.py
@@ -3,11 +3,13 @@ import streamlit as st
 low = st.number_input('Low', value = 100.0, min_value = 0.0)
 high = st.number_input('High', value = 100.0, min_value = low)
 diff = high - low
+diff2 = round(diff, 4)
 spread = diff / high
+spread2 = round(spread * 100, 4)
 output = f'''
 ```python
-Diff   = {high} - {low} = {diff}
-Spread = {diff} / {high} = {round(spread, 6)}
+Diff   = {high} - {low} = {diff2}
+Spread = {diff2} / {high} = {spread2}%
 ```
 '''
 st.write(output)
